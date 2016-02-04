f6a317ffe5fea850a11f3731ba9a3155e42db00c
Eric Willenson <eric.willenson@gmail.com>
2016-02-04 13:47:23 -0500

    stuff

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,12 @@ import Recorder from '../node_modules/recorderjs/recorder';
 import Tone from '../node_modules/tone/build/Tone';
 import $ from '../node_modules/jquery/dist/jquery';
 
-var sampler = new Tone.Sampler({1: '../assets/boing.wav'}).toMaster();
-//var recorder = new Recorder();
+var sampler = new Tone.Sampler({
+  1: '../assets/audio/505/kick.mp3',
+  2: '../assets/audio/505/snare.mp3',
+  3: '../assets/audio/505/hh.mp3',
+  4: '../assets/audio/505/hho.mp3'
+}).toMaster();
 
 /**
  * event handling
@@ -11,11 +15,22 @@ var sampler = new Tone.Sampler({1: '../assets/boing.wav'}).toMaster();
  * controls what happens when any of the buttons are hit
  *
  */
-$('#one').on('click', (e) => {
-  console.log('you clicked button one');
-});
-Tone.Buffer.onLoad = () => {
+Tone.Buffer.on('load', () => {
   $('#one').on('click', (e) => {
+    console.log('you clicked button one');
     sampler.triggerAttack('1');
-  })
-}
+  }); 
+  $('#two').on('click', (e) => {
+    console.log('you clicked button two');
+    sampler.triggerAttack('2');
+  }); 
+  $('#three').on('click', (e) => {
+    console.log('you clicked button three');
+    sampler.triggerAttack('3');
+  }); 
+  $('#four').on('click', (e) => {
+    console.log('you clicked button four');
+    sampler.triggerAttack('4');
+  });
+var recorder = new Recorder(Tone.Master);
+});
