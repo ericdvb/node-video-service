4d27c3a9f1429fc299229885226290fe5ff03937
Eric <e.willenson@bigspaceship.com>
2016-02-08 12:41:48 -0500

    Adding Recorderjs from lib, add browserify to package develop deps, update main.js with working recorder

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import Recorder from '../node_modules/recorderjs/recorder';
+import Recorder from './Recorderjs/lib/index';
 import Tone from '../node_modules/tone/build/Tone';
 import $ from '../node_modules/jquery/dist/jquery';
 
@@ -8,6 +8,10 @@ var sampler = new Tone.Sampler({
   3: '../assets/audio/505/hh.mp3',
   4: '../assets/audio/505/hho.mp3'
 }).toMaster();
+var recorderInput = Tone.context.createGain();
+var recorder = new Recorder(recorderInput);
+var player = new Tone.Player().toMaster();
+sampler.connect(recorderInput);
 
 /**
  * event handling
@@ -16,21 +20,40 @@ var sampler = new Tone.Sampler({
  *
  */
 Tone.Buffer.on('load', () => {
+
   $('#one').on('click', (e) => {
     console.log('you clicked button one');
     sampler.triggerAttack('1');
   }); 
+
   $('#two').on('click', (e) => {
     console.log('you clicked button two');
     sampler.triggerAttack('2');
   }); 
+
   $('#three').on('click', (e) => {
     console.log('you clicked button three');
     sampler.triggerAttack('3');
   }); 
+
   $('#four').on('click', (e) => {
     console.log('you clicked button four');
     sampler.triggerAttack('4');
   });
-var recorder = new Recorder(Tone.Master);
+
+  $('#record').on('click', (e) => {
+    if(recorder.recording === false) {
+      recorder.record();
+      console.log('is recorder recording? ' + recorder.recording);
+    } else if (recorder.recording === true) {
+      recorder.stop();
+      recorder.getBuffer( (buffers) => {
+        var playerBuffer = Tone.context.createBuffer( 2, buffers[0].length, Tone.context.sampleRate ); 
+        playerBuffer.getChannelData(0).set(buffers[0]);
+        playerBuffer.getChannelData(1).set(buffers[1]);
+        player.buffer = playerBuffer;
+        player.start();
+      });
+    }
+  })
 });
