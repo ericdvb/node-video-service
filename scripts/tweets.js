20a8c59ac97d061548a21cad95e3d9d410000731
Eric Willenson <eric.willenson@gmail.com>
2016-03-12 12:35:04 -0500

    Refactor functionality, change route structure
    Moved twitter functionality into tweets.js module exports, move route
    declaration back to server.js, move delayed-response handlers into
    delayedResponseHandlers.js, change route structure to single route
    /video

--- scripts/tweets.js
+++ scripts/tweets.js
@@ -1,67 +1,67 @@
 module.exports = function(router) {
 
-var Twit = require('twit');
-var bodyParser = require('body-parser');
-var DelayedResponse = require('http-delayed-response'); 
+  var Twit = require('twit');
+  var bodyParser = require('body-parser');
+  var DelayedResponse = require('http-delayed-response'); 
 
-// Twit setup
-var twitterSettings = require('../settings.json').twitter;
-var twitterClient = new Twit(twitterSettings);
+  // Twit setup
+  var twitterSettings = require('../settings.json').twitter;
+  var twitterClient = new Twit(twitterSettings);
 
-// Create route for POSTing new media to media/update twitter endpoint
-// and then posting status update
-router.post( '/tweets', bodyParser.text(), (req, res, next) => {
-  console.log('request to tweet video');
-  
-  // create our response promise
-  // set the content-type header to application/json using .json()
-  var delayedResponse = new DelayedResponse(req, res).json();
-  
-  // transform what we got in the request into the filepath
-  var filePath = __dirname + '/../uploads/' + req.body;
-  filePath = __dirname + '/../uploads/1457557560220.mp4';
+  /**
+   * function uploadMedia
+   * Uploads specified file to the twitter /media/upload endpoint
+   *
+   */
+  var uploadMedia = function(filepath) {
 
-  console.log('path to video to upload: ' + filePath);
-
-
-  var twitterPromise = new Promise(function(resolve, reject) {
-    // start the request to twitter endpoint...
-    try {
-      twitterClient.postMediaChunked({ file_path: filePath}, function (error, data, response) {
-        delayedResponse.end(error);
-        if(error) {
-          console.log(error);
-          reject(error)
-        } else {
-          // use if you need to see response data for some reason
-          //console.log(response);
-          //console.log(data);
-          resolve(data.media_id_string);
-        }
+    return new Promise(function(resolve, reject) {
+      // transform what we got in the request into the filepath
+      console.log(filepath);
+      var filePath = filepath;
+      resolve({
+        success: true,
+        error: null,
+        media_id_string: '708406346270576641'
       });
-    }
-    catch(error) {
-      console.log(error);
-      delayedResponse.end(error, null)
-      reject(error);
-    }
-  }).then( function(mediaIDString) {
-    return new Promise( function(resolve, reject) {
+
+      //try {
+        //twitterClient.postMediaChunked({ file_path: filePath}, function (error, data, response) {
+          //var result = {
+            //success: !error ? true : false,
+            //error: error,
+            //media_id_string: data.media_id_string
+          //};
+          //!error ? resolve(result) : reject(result);
+        //});
+      //}
+      //catch(error) {
+        //console.log(error);
+        //resolve(error);
+      //}
+    });
+  };
+
+  var tweetStatusWithVideo = function(mediaIDString, requestBody) {
+
+    return new Promise(function(resolve, reject) {
       try {
+        // set up resources we need for the status update
+        var handleString = requestBody.twitterName;
+        var statusString = 'Bite my shiny, metal ass, ' + handleString;
+
         twitterClient.post('statuses/update',
           { 
-            status: 'Bite my shiny, metal ass!',
+            status: statusString,
             media_ids: [mediaIDString]
           }, 
           function(error, data, response) {
-            delayedResponse.end(error, data);
-            if(!error) {
-              console.log(data);
-              resolve(data.id);
-            } else {
-              console.log(error);
-              reject(error);
-            }
+            var result = {
+              success: !error ? true : false,
+              error: error,
+              tweet_id: data.id
+            };
+            !error ? resolve(result) : reject(result);
           });
       }
       catch (error) {
@@ -69,35 +69,11 @@ router.post( '/tweets', bodyParser.text(), (req, res, next) => {
         reject(error);
       }
     });
-  });
+  };
 
-  delayedResponse.on('done', function(results) {
-    var responseJSON = {
-      success: true,
-      tweet_id: results.id
-    }
-    res.write( JSON.stringify( responseJSON ) );
-    res.end();
-  })
-  .on('error', function(error) {
-    var responseJSON = {
-      success: false,
-      error: error
-    }
-    res.status(error.statusCode);
-    res.write( JSON.stringify( responseJSON ) );
-    res.end();
-  })
-  .on('cancel', function() {
-    var responseJSON = {
-      success: false,
-      error: 'Error: response took too long' 
-    }
-    res.status(504);
-    res.write( JSON.stringify( responseJSON ) );
-    res.end();
-  });
 
-  delayedResponse.start(10000, 10000);
-});
+  return {
+    tweetStatusWithVideo: tweetStatusWithVideo,
+    uploadMedia: uploadMedia
+  };
 };
